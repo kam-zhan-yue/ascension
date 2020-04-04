3a7bb6846c40a8b9f5286431594fcd791a7101db
kam-zhan-yue <zkam02@aism.edu.my>
2020-04-04 19:05:57 +0800

    Menu and Player UI

---
+++ Ascension Prototype/Assets/Scripts/Player Control/HealthUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    public GameObject[] heart = new GameObject[5];
+    private Animator[] heartAnim = new Animator[5];
+
+    private void Start()
+    {
+        for (int i = 0; i < heart.Length; i++)
+        {
+            //Error Prevention
+            if(heart[i]!=null)
+                heartAnim[i] = heart[i].GetComponent<Animator>();
+        }
+    }
+
+    public void Hurt(int health)
+    {
+        heartAnim[health - 1].SetBool("hit", true);
+    }
+
+}
---
+++ Ascension Prototype/Assets/Scripts/Scene Control/PauseMenu.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+    public static bool paused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+
+    }
+
+    //===============PROCEDURE===============//
+    void Pause()
+    //Purpose:          Stops time and shows pause window
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+        Debug.Log("Pausing the game...");
+    }
+
+    //===============PROCEDURE===============//
+    public void Resume()
+    //Purpose:          Resumes time and closes pause window if button is chosen
+    {
+        PauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+        Debug.Log("Resuming the game...");
+    }
+
+    //===============PROCEDURE===============//
+    public void LoadMenu()
+    //Purpose:          Loads the menu scene if button is chosen
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Bringing to the menu...");
+    }
+
+    //===============PROCEDURE===============//
+    public void Quit()
+    //Purpose:          Quits the application if button is chosen
+    {
+        Debug.Log("Exiting the game...");
+        Application.Quit();
+    }
+}
